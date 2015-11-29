25964a5257ee1c378ea757b699a64f5f54e9eff7
Christina Moulton <christina@teakmobile.com>
2015-11-29 10:41:07 -0400

    Added Gist model class

---
+++ grokSwiftREST/grokSwiftREST/Gist.swift
@@ -0,0 +1,29 @@
+//
+//  Gist.swift
+//  grokSwiftREST
+//
+//  Created by Christina Moulton on 2015-11-29.
+//  Copyright © 2015 Teak Mobile Inc. All rights reserved.
+//
+
+import Foundation
+import SwiftyJSON
+
+class Gist {
+  var id: String?
+  var description: String?
+  var ownerLogin: String?
+  var ownerAvatarURL: String?
+  var url: String?
+  
+  required init(json: JSON) {
+    self.description = json["description"].string
+    self.id = json["id"].string
+    self.ownerLogin = json["owner"]["login"].string
+    self.ownerAvatarURL = json["owner"]["avatar_url"].string
+    self.url = json["url"].string
+  }
+  
+  required init() {
+  }
+}
