f66f9330b87f7e89108ca4a432f882f42a1b7cc0
Christina Moulton <christina@teakmobile.com>
2015-11-29 10:46:51 -0400

    Rename objects in MasterViewController to gists. Added dummy gists to test table view can display them

--- grokSwiftREST/grokSwiftREST/MasterViewController.swift
+++ grokSwiftREST/grokSwiftREST/MasterViewController.swift
@@ -9,95 +9,109 @@
 import UIKit
 
 class MasterViewController: UITableViewController {
-
+  
   var detailViewController: DetailViewController? = nil
-  var objects = [AnyObject]()
-
-
+  var gists = [Gist]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     self.navigationItem.leftBarButtonItem = self.editButtonItem()
-
+    
     let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
     self.navigationItem.rightBarButtonItem = addButton
     if let split = self.splitViewController {
-        let controllers = split.viewControllers
-        self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+      let controllers = split.viewControllers
+      self.detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
     }
   }
-
+  
   override func viewWillAppear(animated: Bool) {
     self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
     super.viewWillAppear(animated)
   }
   
   func loadGists() {
-    GitHubAPIManager.sharedInstance.printPublicGists()
+    let gist1 = Gist()
+    gist1.description = "The first gist"
+    gist1.ownerLogin = "gist1Owner"
+    let gist2 = Gist()
+    gist2.description = "The second gist"
+    gist2.ownerLogin = "gist2Owner"
+    let gist3 = Gist()
+    gist3.description = "The third gist"
+    gist3.ownerLogin = "gist3Owner"
+    gists = [gist1, gist2, gist3]
+    // Tell the table view to reload
+    self.tableView.reloadData()
   }
   
   override func viewDidAppear(animated: Bool) {
     super.viewDidAppear(animated)
     loadGists()
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
+  
   func insertNewObject(sender: AnyObject) {
-    objects.insert(NSDate(), atIndex: 0)
-    let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-    self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+    let alert = UIAlertController(title: "Not Implemented", message: "Can't create new gists yet, will implement later", preferredStyle: UIAlertControllerStyle.Alert)
+    alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+    self.presentViewController(alert, animated: true, completion: nil)
   }
-
+  
   // MARK: - Segues
-
+  
   override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
     if segue.identifier == "showDetail" {
-        if let indexPath = self.tableView.indexPathForSelectedRow {
-            let object = objects[indexPath.row] as! NSDate
-            let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
-            controller.detailItem = object
-            controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
-            controller.navigationItem.leftItemsSupplementBackButton = true
+      if let indexPath = self.tableView.indexPathForSelectedRow {
+        let gist = gists[indexPath.row] as Gist
+        if let detailViewController = (segue.destinationViewController as! UINavigationController).topViewController as? DetailViewController {
+          detailViewController.detailItem = gist
+          detailViewController.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+          detailViewController.navigationItem.leftItemsSupplementBackButton = true
         }
+      }
     }
   }
-
+  
   // MARK: - Table View
-
+  
   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
     return 1
   }
-
+  
   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return objects.count
+    return gists.count
   }
-
+  
   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
-
-    let object = objects[indexPath.row] as! NSDate
-    cell.textLabel!.text = object.description
+    
+    let gist = gists[indexPath.row]
+    cell.textLabel!.text = gist.description
+    cell.detailTextLabel!.text = gist.ownerLogin
+    // TODO: set cell.imageView to display image at gist.ownerAvatarURL
+          
     return cell
   }
-
+  
   override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return false if you do not want the specified item to be editable.
-    return true
+        // Return false if you do not want the specified item to be editable.
+        return true
   }
-
+  
   override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
     if editingStyle == .Delete {
-        objects.removeAtIndex(indexPath.row)
-        tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+      gists.removeAtIndex(indexPath.row)
+      tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
     } else if editingStyle == .Insert {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+      // Create a new instance of the appropriate class, insert it into the array,
+      // and add a new row to the table view.
     }
   }
-
-
+  
 }
 
