779659e153d1e198398703b8863c2b8653254898
Christina Moulton <christina@teakmobile.com>
2015-12-01 10:03:01 -0400

    Added table view data source and delegate methods to show gist details in DetailViewController.

--- grokSwiftREST/grokSwiftREST/DetailViewController.swift
+++ grokSwiftREST/grokSwiftREST/DetailViewController.swift
@@ -20,10 +20,8 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
   
   func configureView() {
     // Update the user interface for the detail item.
-    if let currentGist = self.gist {
-      if let label = self.detailDescriptionLabel {
-        label.text = currentGist.description
-      }
+    if let detailsView = self.tableView {
+      detailsView.reloadData()
     }
   }
 
@@ -38,6 +36,42 @@ class DetailViewController: UIViewController, UITableViewDataSource, UITableView
     // Dispose of any resources that can be recreated.
   }
 
-
+  // MARK: - Table view data source and delegate
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 2
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if section == 0 {
+      return 2
+    } else {
+      return gist?.files?.count ?? 0
+    }
+  }
+  
+  func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    if section == 0 {
+      return "About"
+    } else {
+      return "Files"
+    }
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+    
+    if indexPath.section == 0 {
+      if indexPath.row == 0 {
+        cell.textLabel?.text = gist?.description
+      } else if indexPath.row == 1 {
+        cell.textLabel?.text = gist?.ownerLogin
+      }
+    } else {
+      if let file = gist?.files?[indexPath.row] {
+        cell.textLabel?.text = file.filename
+      }
+    }
+    return cell
+  }
 }
 
