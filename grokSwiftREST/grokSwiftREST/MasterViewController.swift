05581568a2172b8311fee7f9d78c50b66a44d916
Christina Moulton <christina@teakmobile.com>
2015-12-03 09:05:03 -0400

    Added PersistenceManager to save and load previously fetched gists if no internet connection.

--- grokSwiftREST/grokSwiftREST/MasterViewController.swift
+++ grokSwiftREST/grokSwiftREST/MasterViewController.swift
@@ -76,6 +76,20 @@ class MasterViewController: UITableViewController, LoginViewDelegate, SFSafariVi
             if error.code == NSURLErrorUserAuthenticationRequired {
               self.showOAuthLoginView()
             } else if error.code == NSURLErrorNotConnectedToInternet {
+              let path:Path
+              if self.gistSegmentedControl.selectedSegmentIndex == 0 {
+                path = .Public
+              } else if self.gistSegmentedControl.selectedSegmentIndex == 1 {
+                path = .Starred
+              } else {
+                path = .MyGists
+              }
+              if let archived:[Gist] = PersistenceManager.loadArray(path) {
+                self.gists = archived
+              } else {
+                self.gists = [] // don't have any saved gists
+              }
+              
               // show not connected error & tell em to try again when they do have a connection
               // check for existing banner
               if let existingBanner = self.notConnectedBanner {
@@ -101,6 +115,16 @@ class MasterViewController: UITableViewController, LoginViewDelegate, SFSafariVi
           self.gists = fetchedGists
         }
       }
+      
+      let path:Path
+      if self.gistSegmentedControl.selectedSegmentIndex == 0 {
+        path = .Public
+      } else if self.gistSegmentedControl.selectedSegmentIndex == 1 {
+        path = .Starred
+      } else {
+        path = .MyGists
+      }
+      PersistenceManager.saveArray(self.gists, path: path)
       
       // update "last updated" title for refresh control
       let now = NSDate()
--- grokSwiftREST/grokSwiftREST/PersistenceManager.swift
+++ grokSwiftREST/grokSwiftREST/PersistenceManager.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+enum Path: String {
+  case Public = "Public"
+  case Starred = "Starred"
+  case MyGists = "MyGists"
+}
+
+class PersistenceManager {
+  class private func documentsDirectory() -> NSString {
+    let paths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory,
+      .UserDomainMask, true)
+    let documentDirectory = paths[0] as String
+    return documentDirectory
+  }
+  
+  class func saveArray<T: NSCoding>(arrayToSave: [T], path: Path) {
+    let file = documentsDirectory().stringByAppendingPathComponent(path.rawValue)
+    NSKeyedArchiver.archiveRootObject(arrayToSave, toFile: file)
+  }
+  
+  class func loadArray<T: NSCoding>(path: Path) -> [T]? {
+    let file = documentsDirectory().stringByAppendingPathComponent(path.rawValue)
+    let result = NSKeyedUnarchiver.unarchiveObjectWithFile(file)
+    return result as? [T]
+  }
+}
